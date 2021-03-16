1661014be4a16d56637e45959c0377189108aa73
Matthew Craven <mcrav@protonmail.com>
2021-03-16 20:04:39 +0000

    Converted to use Prettier code formatting

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,13 @@
 export enum Routes {
-  Login = '/login',
-  PasswordHealth = '/items',
-  Weak = '/items/weak',
-  Reused = '/items/reused',
-  Root = '/',
+  Login = "/login",
+  PasswordHealth = "/items",
+  Weak = "/items/weak",
+  Reused = "/items/reused",
+  Root = "/",
 }
 
 export enum API {
-  Login = 'api/login',
-  Items = 'api/items',
-  User = 'api/user',
+  Login = "api/login",
+  Items = "api/items",
+  User = "api/user",
 }
